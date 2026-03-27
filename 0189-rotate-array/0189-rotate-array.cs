45d373ddbfb9286e001d49248c9915555c169c3b
kaitouK <71955159+kaitouK@users.noreply.github.com>
2026-03-27 16:34:59 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cs
+++ 0189-rotate-array/0189-rotate-array.cs
@@ -1,17 +1,16 @@
 public class Solution {
     public void Rotate(int[] nums, int k) {
         int n=nums.Length;
+        if(n<=1)return;
         k%=n;
+        if(k==0)return;
         Reverse(nums,0,n-1);
         Reverse(nums,0,k-1);
         Reverse(nums,k,n-1);
     }
     private void Reverse(int[] num, int start,int end)
     {
-        while(start<end){
-            (num[start],num[end])=(num[end],num[start]);
-            --end;
-            ++start;
-        }
+        while(start<end)
+            (num[start],num[end])=(num[end--],num[start++]);
     }
 }
