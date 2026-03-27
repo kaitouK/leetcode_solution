b7ae5e0902905ac603f4de8aafee6477fdcf45ec
kaitouK <71955159+kaitouK@users.noreply.github.com>
2026-03-27 11:37:14 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cs
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cs
@@ -4,38 +4,18 @@ public class Solution {
         k%=m;
         if(k==0)
         return true;
-        int[][]newmat=new int[n][];
-        for(int i=0;i<n;++i)
-        newmat[i]=(int[])mat[i].Clone();
-        
-        // 3 step rotation
         for(int i=0;i<n;++i){
-            if(i%2==0)//left rotate k times
-            {
-                Reverse(newmat[i],0,k-1);
-                Reverse(newmat[i],k,m-1);
-                Reverse(newmat[i],0,m-1);
-            }
-            else//right rotate k times;
-            {
-                Reverse(newmat[i],0,m-1);
-                Reverse(newmat[i],0,k-1);
-                Reverse(newmat[i],k,m-1);
+            for(int j=0;j<m;++j){
+                if(i%2==0)
+                {
+                    if(mat[i][j]!=mat[i][(j-k+m)%m])return false;
+                }
+                else
+                {
+                    if(mat[i][j]!=mat[i][(j+k)%m])return false;
+                }
             }
         }
-        for(int i=0;i<n;++i)
-        for(int j=0;j<m;++j)
-        if(mat[i][j]!=newmat[i][j])
-        return false;
         return true;
-    }
-    private void Reverse(int[] arr,int start,int end)
-    {
-        while(start<end){
-            (arr[start],arr[end])=(arr[end],arr[start]);
-            ++start;
-            --end;
-        }
-
     }
 }
