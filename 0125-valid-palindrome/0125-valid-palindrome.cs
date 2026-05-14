2eaa3df9e9fb4114c3ac680dc6293a5325561682
kaitouK <71955159+kaitouK@users.noreply.github.com>
2026-05-14 16:38:54 +0800

    Time: 16 ms (67.97%), Space: 44.7 MB (78.58%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        var s1=new StringBuilder();
+        foreach (char c in s){
+            if(Char.IsLetterOrDigit(c))
+            s1.Append(Char.ToLower(c));
+        }
+        int left=0,right=s1.Length-1;
+        while(left<right)
+        {
+            if(s1[left++]!=s1[right--])
+            return false;
+        }
+        return true;
+    }
+}
