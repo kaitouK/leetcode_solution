dfdb55c442670ba174caf7fb44c7a4dd41b941bf
kaitouK <71955159+kaitouK@users.noreply.github.com>
2026-08-10 20:18:29 +0800

    Time: 0 ms (100%), Space: 12.5 MB (32.46%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (n == 0)
+            return;
+        if (m == 0) {
+            nums1 = nums2;
+            return;
+        }
+        for (int i = 0; i < n; ++i) {
+            int left = 0, right = m + i - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                    nums1[mid] >= nums2[i] ? right = mid - 1 : left = mid + 1;
+            }
+            shift(nums1, left, m + i);
+            nums1[left] = nums2[i];
+        }
+    }
+    void shift(vector<int>& nums1, int left, int right) {
+        if (left == right)
+            return;
+        while (left < right) {
+            swap(nums1[right], nums1[right - 1]);
+            right--;
+        }
+    }
+};
