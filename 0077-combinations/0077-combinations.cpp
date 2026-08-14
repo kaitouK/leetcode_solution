0d05fbcdc1e80579b14f15f81bee740196f51f55
kaitouK <71955159+kaitouK@users.noreply.github.com>
2026-08-14 20:26:19 +0800

    Time: 74 ms (36.94%), Space: 82.3 MB (56.96%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-vector<int> path;
-vector<vector<int>> ans;
-int k,n;
-void dfs(int start)
-{
-    if(path.size()==k)
-    {ans.push_back(path);return;}
-    for(int i=start;i<=n;++i){
-        path.push_back(i);
-        dfs(i+1);
-        path.pop_back();
+    vector<int> path;
+    vector<vector<int>> ans;
+    int k, n;
+    void dfs(int start) {
+        if (path.size() == k) {
+            ans.push_back(path);
+            return;
+        }
+        int need = k - path.size();
+        for (int i = start; i <= n - need + 1; ++i) {
+            path.push_back(i);
+            dfs(i + 1);
+            path.pop_back();
+        }
     }
-
-}
     vector<vector<int>> combine(int _n, int _k) {
-        k=_k,n=_n;
+        k = _k, n = _n;
+        path.clear();
+        ans.clear();
         dfs(1);
         return ans;
-        
     }
 };
